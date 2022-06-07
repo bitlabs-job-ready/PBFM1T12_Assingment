5b3a9ddf39f537f5437aaa49351c0ba0f6febcec
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-06-07 17:23:10 +0530

    Add files via upload

---
+++ PBFM1T12_Assingment1.py
@@ -0,0 +1,12 @@
+'''
+write a program that tries to accumulate sum of values based on the input in some dictionaries. 
+Insert a try/except so that the code passes.
+
+Sample Input:
+
+di = [{"Puppies": 17, 'Kittens': 9, "Birds": 23, 'Fish': 90, "Hamsters": 49}, {"Puppies": 23, "Birds": 29, "Fish": 20, "Mice": 20, "Snakes": 7}, {"Fish": 203, "Hamsters": 93, "Snakes": 25, "Kittens": 89}, {"Birds": 20, "Puppies": 90, "Snakes": 21, "Fish": 10, "Kittens": 67}]
+
+Sample Output:
+Total number of puppies:130
+
+'''
